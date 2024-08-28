ca75d031bafdde47fbb9ace90befd0dc960e852c
akulb07 <batraakul08@gmail.com>
2024-08-28 23:36:33 +0530

    u

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,21 +1,31 @@
 <script>
     import Title from '$lib/components/Title.svelte';
     let title = "About me";
-    let friendsTitle = "My friends";
-    let friendName = "Nermal";
-    let friendGitHub = "https://github.com/nermalcat69";
-    let friendImage = "https://eu-central.storage.cloudconvert.com/tasks/8fcb1d2d-4756-4e00-947d-5b0bfc220f09/73933669.webp?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=cloudconvert-production%2F20240525%2Ffra%2Fs3%2Faws4_request&X-Amz-Date=20240525T051356Z&X-Amz-Expires=86400&X-Amz-Signature=4eeca55d950c6fac7d250297348e73e6d3885c3bb44c7ce7bb7177059788dcab&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3D%2273933669.webp%22&response-content-type=image%2Fwebp&x-id=GetObject";
+    let trackTitle = "u ";
+    let artist = "Kendrick Lamar";
+    let albumCover = "https://lastfm.freetls.fastly.net/i/u/770x0/86b35c4eb3c479da49c915d8771bbd1a.jpg#86b35c4eb3c479da49c915d8771bbd1a";
+    let spotifyLink = "https://open.spotify.com/track/1bxEpNR75Hq3T2oF9AZjt8?si=80ffc94f83094d24";
 </script>
 
 <div class="flex justify-center items-center">
-    <div class="w-full max-w-2xl divide-y divide-gray-200 dark:divide-gray-700">
+    <div class="w-full max-w-2xl  divide-gray-200 dark:divide-gray-700">
         <div class="space-y-2 pt-6 pb-8 md:space-y-5">
             <div>
                 <Title {title} />
             </div>
         </div>
         <div class="pt-8">
-            <h1 class="text-2xl">Yeah...I don't really know what to write here.</h1>
+            <h1 class="text-2xl">.</h1>
+        </div>
+        <div class="pt-8">
+            <div class=" rounded-lg  p-6 flex items-center space-x-6">
+                <img src={albumCover} alt="Album cover" class="w-32 h-32 "/>
+                <div>
+                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">{trackTitle}</h2>
+                    <p class="text-gray-500 dark:text-gray-400">{artist}</p>
+                    <a href={spotifyLink} target="_blank" class="text-blue-500 dark:text-blue-400 hover:underline">Listen on Spotify</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
