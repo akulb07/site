caa0089e4abae45e252d82b74c99acbbddb1d9cd
akul batra <batraakul08@gmail.com>
2024-09-26 17:41:56 +0530

    links

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="flex justify-center items-center">
-    <div class="w-full max-w-2xl  divide-gray-200 dark:divide-gray-700">
+    <div class="w-full max-w-2xl divide-gray-200 dark:divide-gray-700">
         <div class="space-y-2 pt-6 pb-8 md:space-y-5">
             <div>
                 <Title {title} />
@@ -12,6 +12,26 @@
         </div>
         <div class="pt-8">
             <h1 class="text-2xl">Definitely one of the people of all time</h1>
+        </div>
+        <!-- Links Section -->
+        <div class="pt-8">
+            <h2 class="text-xl font-bold">Links</h2>
+            <ul class="list-disc list-inside">
+                <li>
+                    <a href="https://lichess.org/@/akulb07" target="_blank" class="text-blue-500 hover:underline">
+                        Lichess
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.hackerrank.com/profile/batraakul07" target="_blank" class="text-blue-500 hover:underline">
+                        HackerRank
+                    </a>
+                </li>
+                <li>
+                    <a href="https://leetcode.com/u/BatraAkul/" target="_blank" class="text-blue-500 hover:underline">
+                        LeetCode
+                    </a>
+            </ul>
         </div>
     </div>
 </div>
