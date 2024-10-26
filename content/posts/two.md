e9d92bb2751d5d6c13b81582aa92308d5b0f7492
akul batra <batraakul08@gmail.com>
2024-10-26 14:17:56 +0530

    new post

---
+++ content/posts/two.md
@@ -0,0 +1,82 @@
+---
+title: "Solving the CSES Problem Set: Day 1"
+date: "2024-10-26"
+draft: false
+---
+
+
+# Introductory Problems
+
+## [Weird Algorithm](https://cses.fi/problemset/task/1068)
+
+In this problem, we're given an integer `n`. If `n` is even, we have to divide it by 2, and if `n` is odd, we have to multiply it by 3 and add 1. This process is to be repeated until `n` is one.
+
+Fairly straightforward, nothing much to say here.
+```py 
+n = int(input(""))
+print(n, end = " ")
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+    else:
+        n =  n*3 + 1
+    print(n, end = " ")
+```
+
+## [Missing Number](https://cses.fi/problemset/task/1083)
+
+In this problem, we’re given a list of numbers from `1` to `n`, with one missing integer. The goal is to find this missing number efficiently.
+
+### Approach 1
+The first approach uses the formula for the sum of the first `n` natural numbers.
+
+By calculating this expected sum and subtracting the sum of the given numbers, we can find the missing number in constant time.
+
+```python
+n = int(input(""))
+l = list(map(int, input("").split()))
+print((n * (n + 1)) // 2 - sum(l))
+```
+
+### Approach 2
+The second approach involves using a set to store all the numbers in the input list. We then iterate from `1` to ``n`` and check if each number is in the set. The number that isn't in the set is the missing one.
+
+Here, using a list is not recommended as to check if an item is there in a list, Python must go through each item one by one until it finds a match (or determines that the item is not present), which is extremely slow.
+
+```python
+n = int(input(""))
+numbers = set(map(int, input().split()))
+ 
+for i in range(1, n + 1):
+    if i not in numbers:
+        print(i)
+```
+
+## [Repetitions](https://cses.fi/problemset/task/1069)
+
+In this problem, we have to find the length of the longest consecutive sequence of identical characters in a given string.
+
+This problem is also fairly simple, we can iterate through the string comparing each character to the previous one.
+
+We will need two variables:  one to track the current count of consecutive repetitions and another to store the maximum count found so far.
+
+Here, we cannot check if the current character is equal to the next one (`n[i] == n[i+1]`), because when we reach the last character (at index `len(n) - 1`), trying to access `n[i+1]` would result in an out-of-bounds error.
+
+
+```python
+n = input("")
+count = 1
+max_l = 1
+
+for i in range(1, len(n)):
+    if n[i] == n[i-1]:
+        count += 1
+        max_l = max(count, max_l)
+    else:
+        count = 1
+print(max_l)
+```
+
+
+This concludes this post. I will be writing more as I solve additional problems. You can also check out my solutions in my [GitHub repository](https://github.com/akulb07/CompetitiveProgramming), which I will be updating regularly.
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,21 +7,21 @@ module.exports = {
 	theme: {
 		extend: {
 			spacing: {
-				'9/16': '56.25%'
+				'9/16': '56.25%',
 			},
 			lineHeight: {
 				11: '2.75rem',
 				12: '3rem',
 				13: '3.25rem',
-				14: '3.5rem'
+				14: '3.5rem',
 			},
 			fontFamily: {
 				title: ['Jetbrains Mono', ...defaultTheme.fontFamily.mono],
-				body: ['Jetbrains mono', ...defaultTheme.fontFamily.mono]
+				body: ['Jetbrains Mono', ...defaultTheme.fontFamily.mono],
 			},
 			colors: {
 				primary: colors.blue,
-				gray: colors.zinc
+				gray: colors.zinc,
 			},
 			typography: (theme) => ({
 				DEFAULT: {
@@ -30,41 +30,41 @@ module.exports = {
 						a: {
 							color: theme('colors.primary.600'),
 							'&:hover': {
-								color: `${theme('colors.primary.800')} !important`
+								color: `${theme('colors.primary.800')} !important`,
 							},
-							code: { color: theme('colors.primary.400') }
+							code: { color: theme('colors.primary.400') },
 						},
 						h1: {
 							fontWeight: '800',
 						},
 						h2: {
 							fontWeight: '800',
-							color: theme('colors.gray.900')
+							color: theme('colors.gray.900'),
 						},
 						h3: {
 							fontWeight: '800',
-							color: theme('colors.gray.900')
+							color: theme('colors.gray.900'),
 						},
 						'h4,h5,h6': {
-							color: theme('colors.gray.900')
+							color: theme('colors.gray.900'),
 						},
 						pre: {
-							backgroundColor: theme('colors.gray.800')
+							backgroundColor: theme('colors.gray.800'),
 						},
 						code: {
 							color: theme('colors.blue.500'),
-							backgroundColor: theme('colors.gray.100'),
+							backgroundColor: theme('colors.gray.800'),
 							paddingLeft: '4px',
 							paddingRight: '4px',
 							paddingTop: '2px',
 							paddingBottom: '2px',
-							borderRadius: '0.25rem'
+							borderRadius: '0.25rem',
 						},
 						'code::before': {
-							content: 'none'
+							content: 'none',
 						},
 						'code::after': {
-							content: 'none'
+							content: 'none',
 						},
 						details: {
 							backgroundColor: theme('colors.gray.100'),
@@ -72,22 +72,22 @@ module.exports = {
 							paddingRight: '4px',
 							paddingTop: '2px',
 							paddingBottom: '2px',
-							borderRadius: '0.25rem'
+							borderRadius: '0.25rem',
 						},
 						hr: { borderColor: theme('colors.gray.200') },
 						'ol li::marker': {
 							fontWeight: '600',
-							color: theme('colors.gray.500')
+							color: theme('colors.gray.500'),
 						},
 						'ul li::marker': {
-							backgroundColor: theme('colors.gray.500')
+							backgroundColor: theme('colors.gray.500'),
 						},
 						strong: { color: theme('colors.gray.600') },
 						blockquote: {
 							color: theme('colors.gray.900'),
-							borderLeftColor: theme('colors.gray.200')
-						}
-					}
+							borderLeftColor: theme('colors.gray.200'),
+						},
+					},
 				},
 				dark: {
 					css: {
@@ -95,60 +95,63 @@ module.exports = {
 						a: {
 							color: theme('colors.primary.500'),
 							'&:hover': {
-								color: `${theme('colors.primary.400')} !important`
+								color: `${theme('colors.primary.400')} !important`,
 							},
-							code: { color: theme('colors.primary.400') }
+							code: {
+								color: theme('colors.primary.400'),
+								backgroundColor: theme('colors.gray.800'),
+							},
 						},
 						h1: {
 							fontWeight: '700',
 							letterSpacing: theme('letterSpacing.tight'),
-							color: theme('colors.gray.100')
+							color: theme('colors.gray.100'),
 						},
 						h2: {
 							fontWeight: '700',
 							letterSpacing: theme('letterSpacing.tight'),
-							color: theme('colors.gray.100')
+							color: theme('colors.gray.100'),
 						},
 						h3: {
 							fontWeight: '600',
-							color: theme('colors.gray.100')
+							color: theme('colors.gray.100'),
 						},
 						'h4,h5,h6': {
-							color: theme('colors.gray.100')
+							color: theme('colors.gray.100'),
 						},
 						pre: {
-							backgroundColor: theme('colors.gray.800')
+							backgroundColor: theme('colors.gray.800'),
 						},
 						details: {
-							backgroundColor: theme('colors.gray.800')
+							backgroundColor: theme('colors.gray.800'),
 						},
 						hr: { borderColor: theme('colors.gray.700') },
 						'ol li::marker': {
 							fontWeight: '600',
-							color: theme('colors.gray.400')
+							color: theme('colors.gray.400'),
 						},
 						'ul li::marker': {
-							backgroundColor: theme('colors.gray.400')
+							backgroundColor: theme('colors.gray.400'),
 						},
 						strong: { color: theme('colors.gray.100') },
 						thead: {
 							th: {
-								color: theme('colors.gray.100')
-							}
+								color: theme('colors.gray.100'),
+							},
 						},
 						tbody: {
 							tr: {
-								borderBottomColor: theme('colors.gray.700')
-							}
+								borderBottomColor: theme('colors.gray.700'),
+							},
 						},
 						blockquote: {
 							color: theme('colors.gray.100'),
-							borderLeftColor: theme('colors.gray.700')
-						}
-					}
-				}
-			})
-		}
+							borderLeftColor: theme('colors.gray.700'),
+						},
+					},
+				},
+			}),
+		},
 	},
-	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
